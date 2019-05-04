fb1120c0e7715eef798efc6c56a3f401513c7883
Sumit Kumar(2986877) <sumit.kumar@studen.griffith.ie>
2019-05-04 14:39:33 +0100

    Removing unused lines from myuser.py

--- myuser.py
+++ myuser.py
@@ -5,7 +5,4 @@ from twitter import Twitter
 class MyUser(ndb.Model):
     tweets = ndb.KeyProperty(kind=Twitter, repeated=True)
     username = ndb.StringProperty()
-    follower = ndb.StringProperty(repeated=True)
     following = ndb.StringProperty(repeated=True)
-    follower_count = ndb.IntegerProperty()
-    following_count = ndb.IntegerProperty()
